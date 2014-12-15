9dc3ac4f903820dbb20e88123989484f533740e3
hoho18 <qcl4563@163.com>
2014-12-16 07:51:28 +0800

    未包括进来的定时器7

---
+++ MorSun.Controllers/Quartz/AncyRCJOB7/CheckingJob7.cs
@@ -0,0 +1,30 @@
+﻿using HOHO18.Common;
+using HOHO18.Common.WEB;
+using Quartz;
+using System;
+
+
+
+namespace MorSun.Controllers.Quartz
+{
+    public class CheckingJob7:IJob
+    {       
+        public void Execute(IJobExecutionContext context)
+        {
+            //LogHelper.Write("定时器开始执行问题数据同步", LogHelper.LogMessageType.Info);
+            //如果是当天的2点到4点，获取昨天所有数据
+            try
+            {          
+                //获取马币充值信息
+                new BasisController().AncyRC();
+                //同步充值马币记录
+                new BasisController().RCMB();
+            }
+            catch
+            {
+                LogHelper.Write("定时器问题数据同步时出现异常", LogHelper.LogMessageType.Info);
+            }
+            //AncyUser(null, "");
+        }
+    }
+}
---
+++ MorSun.Controllers/Quartz/AncyRCJOB7/CheckingTrigger7.cs
@@ -0,0 +1,26 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MorSun.Controllers.Quartz
+{
+    public class CheckingTrigger7
+    {
+        public virtual void Run()
+        {            
+
+            IJobDetail job = JobBuilder.Create<CheckingJob7>()
+                .WithIdentity("job77", "group77")//.RequestRecovery(true)//服务重启之后不用再执行任务 应用重启之后时候忽略过期任务，默认false
+                .Build();
+
+            ICronTrigger trigger = (ICronTrigger)TriggerBuilder.Create()
+                                                      .WithIdentity("trigger77", "group77")
+                                                      .WithCronSchedule("0/20/40 * * * * ?")//.WithCronSchedule("20 30 9,14,22 * * ?")
+                                                      .Build();
+
+            DateTimeOffset ft = MorSunScheduler.Instance.SchedulerJob(job, trigger);            
+        }
+    }
+}
